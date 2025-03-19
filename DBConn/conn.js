9d85e1af1fc09b461c861e9cd9c12d8f59d9f603
Varun Devidas Kamathi <121332863+varunkamathi@users.noreply.github.com>
2025-03-19 13:41:30 +0530

    Delete DBConn directory

--- DBConn/conn.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect('mongodb://localhost:27017/gymBackend')
-.then(() => console.log('DB connection successful!')).catch(err=>{
-  console.log(err)
-});
